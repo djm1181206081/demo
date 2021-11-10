5b9b7ee9cdc77f711dce6f933ad9b8983c7264c0
djm1181206081 <92069605+djm1181206081@users.noreply.github.com>
2021-11-10 17:28:55 +0800

    Add files via upload

--- 工商银行完整版1.py
+++ 工商银行完整版1.py
@@ -1,329 +1,137 @@
-# author:jason
-import random
+"""
+@author:96978
+@file:day14.py
+@time:2021/11/09
+"""
 
-# 银行库
-bank = {
-    '123': {
-        'account': 'TGutLVIE',
-        'password': '123',
-        'country': '中国',
-        'province': '山西',
-        'street': '桃源里',
-        'door': 's001',
-        'money': 10000,
-        'bank_name': '中国工商银行昌平支行'
-    },
-    '234': {
-        'account': 'TGutLVas',
-        'password': '123',
-        'country': '中国',
-        'province': '山西',
-        'street': '桃源里',
-        'door': 's001',
-        'money': 0,
-        'bank_name': '中国工商银行昌平支行'
-    },
-    '345': {
-        'account': 'TGutKOSA',
-        'password': '123',
-        'country': '中国',
-        'province': '山西',
-        'street': '桃源里',
-        'door': 's001',
-        'money': 10000,
-        'bank_name': '中国工商银行昌平支行'
-    },
+from unittest import TestCase
+from ddt import ddt
+from ddt import data
+from ddt import unpack
+from 工商银行完整版 import bank_saveMoney
+from 工商银行完整版 import bank_takeMoney
+from 工商银行完整版 import bank_selectUser
+from 工商银行完整版 import bank_transformMoney
 
-}
-bank_name = "中国工商银行昌平支行"
-bank_choice = {"1": "开户", "2": "存钱", "3": "取钱", "4": "转账", "5": "查询", "6": "Bye"}  # 银行业务选项
-# 开户成功的信息模板
-myinfo = '''
-    \033[0;32;40m
-    ------------账户信息------------
-    账号：{account}
-    姓名：{username}
-    密码：{password}
-    地址：
-        国家：{country}
-        省份：{province}
-        街道：{street}
-        门牌号：{door}
-    账户余额：{money}
-    注册银行名：{bank_name}
-    -------------------------------
-    \033[0m
-'''
-
-# 欢迎模板
-welcome = '''
-***********************************
-*      中国工商银行账户管理系统       *
-***********************************
-*               选项              *
-'''
-
-welcome_item = '''*              {0}.{1}             *'''
+import xlrd
+from xlutils import copy
 
 
-def print_welcome():
-    print(welcome, end="")
-    keys = bank_choice.keys()
-    for i in keys:
-        print(welcome_item.format(i, bank_choice[i]))
-    print("**********************************")
+de=xlrd.open_workbook(filename='D:\Pythonproject\pythonproject1\pycode1\data.xls',encoding_override=True)
 
 
-# 输入帮助方法：chose是打印选项
-def inputHelp(chose, datatype="str"):
-    while True:
-        print("请输入", chose, ":")
-        i = input(">>>:")
-        if len(i) == 0:
-            print("该项不能为空！请重新输入！")
-            continue
-        if datatype != "str":
-            return int(i)
-        else:
-            return i
+sheet=de.sheet_by_index(0)
+cols=sheet.ncols
+rows=sheet.nrows
+list=[]
+for i in range(1,rows):
+	list1=[]
+	for j in range(cols-1):
+		va=sheet.cell_value(i,j)
+		list1.append(va)
+	list.append(list1)
 
 
-# 判断是否存在该银行选项
-def isExists(chose, data):
-    if chose in data:
-        return True
-    return False
+sheet1=de.sheet_by_index(1)
+cols1=sheet1.ncols
+rows1=sheet1.nrows
+list2=[]
+for i in range(1,rows1):
+	list1=[]
+	for j in range(cols1-1):
+		va=sheet1.cell_value(i,j)
+		list1.append(va)
+	list2.append(list1)
 
 
-# 获取随机码
-def getRandom():
-    li = "0123456789qwertyuiopasdfghjklzxcvbnmZXCVBNMASDFGHJKLQWERTYUIOP"
-    string = ""
-    for i in range(8):
-        string = string + li[int(random.random() * len(li))]
-    return string
+sheet2=de.sheet_by_index(2)
+cols2=sheet2.ncols
+rows2=sheet2.nrows
+list3=[]
+for i in range(1,rows2):
+	list1=[]
+	for j in range(cols2-1):
+		va=sheet2.cell_value(i,j)
+		list1.append(va)
+	list3.append(list1)
 
 
-# 通过账号获取账户信息
-def findByAccount(account):
-    for i in bank.keys():
-        if bank[i]["account"] == account:
-            return i
-    return None
+sheet3=de.sheet_by_index(3)
+cols3=sheet3.ncols
+rows3=sheet3.nrows
+list4=[]
+for i in range(1,rows3):
+	list1=[]
+	for j in range(cols3-1):
+		va=sheet3.cell_value(i,j)
+		list1.append(va)
+	list4.append(list1)
 
 
-# 银行的开户方法
-def bank_addUser(username, password, country, province, street, door, money):
-    # 查询是否已满
-    if len(bank) >= 100:
-        return 3
 
-    # 查询是否存在
-    if username in bank:
-        return 2
+@ddt
+class testCase(TestCase):
+	@data(*list)
+	@unpack
+	def testSaveMoney(self,ac,money,a,row):
+		re=bank_saveMoney(ac,money)
+		#断言
+		de1 = xlrd.open_workbook(filename='D:\Pythonproject\pythonproject1\pycode1\data.xls', encoding_override=True)
+		workbook=copy.copy(de1)
+		worksheet=workbook.get_sheet(0)
+		if re==a :
+			worksheet.write(row,4,'通过')
+			workbook.save('data.xls')
+		else:
+			worksheet.write(row,4,'未通过')
+			workbook.save('data.xls')
 
-    # 插入数据
+	@data(*list2)
+	@unpack
+	def testTakeMoney(self, ac,password,money, a, row):
+		re = bank_takeMoney(ac, password,money)
+		# 断言
+		de1 = xlrd.open_workbook(filename='D:\Pythonproject\pythonproject1\pycode1\data.xls',
+								 encoding_override=True)
+		workbook = copy.copy(de1)
+		worksheet = workbook.get_sheet(1)
+		if re == a:
+			worksheet.write(row, 5, '通过')
+			workbook.save('data.xls')
+		else:
+			worksheet.write(row, 5, '未通过')
+			workbook.save('data.xls')
 
-    bank[username] = {
-        "account": getRandom(),
-        "password": password,
-        "country": country,
-        "province": province,
-        "street": street,
-        "door": door,
-        "money": money,
-        "bank_name": bank_name}
-    return 1
-
-
-# 银行的存钱方法
-def bank_saveMoney(ac, money):
-    for i in bank.keys():
-        if bank[i]["account"] == ac:
-            # print(bank[i]["money"])
-            bank[i]["money"] += money
-            return True
-    return False
-
-
-# 银行的查询功能
-def bank_selectUser(account, password):
-    uname = findByAccount(account)
-
-    if uname != None and len(uname) != 0:
-        if password == bank[uname]["password"]:
-            user = bank[uname]
-            print(myinfo.format(account=user["account"],
-                                username=uname,
-                                password=user["password"],
-                                country=user["country"],
-                                province=user["province"],
-                                street=user["street"],
-                                door=user["door"],
-                                money=user["money"],
-                                bank_name=user["bank_name"]
-                                ))
-            return 0
-
-        else:
-            print("用户密码错误！")
-            return 2
-    else:
-        print("该用户不存在！")
-        return 1
+	@data(*list3)
+	@unpack
+	def testSelectUser(self, ac,password, a, row):
+		re = bank_selectUser(ac, password)
+		# 断言
+		de1 = xlrd.open_workbook(filename='D:\Pythonproject\pythonproject1\pycode1\data.xls',
+								 encoding_override=True)
+		workbook = copy.copy(de1)
+		worksheet = workbook.get_sheet(2)
+		if re == a:
+			worksheet.write(row, 4, '通过')
+			workbook.save('data.xls')
+		else:
+			worksheet.write(row, 4, '未通过')
+			workbook.save('data.xls')
 
 
-# 银行的取钱功能
-def bank_takeMoney(account, password, money):
-    uname = findByAccount(account)
-    if uname != None:
-        if bank[uname]["password"] == password:
-            if bank[uname]["money"] < money:
-                return 3
-            else:
-                bank[uname]["money"] -= money
-                return 0
-        else:
-            return 2
-    else:
-        return 1
 
-
-# 银行的转账功能
-def bank_transformMoney(outputaccount, inputaccount, outputpassword, outputmoney):
-    status = bank_takeMoney(outputaccount, outputpassword, outputmoney)
-    if status == 1:
-        return status
-    elif status == 2:
-        return status
-    elif status == 3:
-        return status
-
-    if inputaccount != None and findByAccount(inputaccount) != None:
-        bank_saveMoney(inputaccount, outputmoney)
-        return 0
-    else:
-        return 1
-
-# # 开户方法
-# def addUser():
-#     username = inputHelp("用户名")
-#     password = inputHelp("密码")
-#     country = inputHelp("居住地址：1.国家：")
-#     province =  inputHelp("省份")
-#     street = inputHelp("街道")
-#     door = inputHelp("门牌号")
-#     money =  inputHelp("银行卡余额","int")
-#
-#     # 调用银行的开户方法完成开户操作  返回 1 2 3
-#     status = bank_addUser(username,password,country,province,street,door,money)
-#     # 判断1   2   3
-#     if status == 1:
-#         user = bank[username]
-#         print("恭喜开户成功！以下是您的开户信息：")
-#         print(myinfo.format(account=user["account"],
-#                             username=username,
-#                             password=user["password"],
-#                             country=user["country"],
-#                             province=user["province"],
-#                             street=user["street"],
-#                             door=user["door"],
-#                             money=user["money"],
-#                             bank_name=user["bank_name"]
-#                             ))
-#     elif status == 2:
-#         print("改用户已经存在！请携带证件到其他银行办理！谢谢！！！！！")
-#     elif status == 3:
-#         print("银行库已满！请携带证件到其他银行办理！谢谢！！！！！")
-#
-# # 存钱
-# def saveMoney():
-#     account = inputHelp("账号")
-#     m =  inputHelp("存入的金额","int")
-#
-#     flag = bank_saveMoney(account,m)
-#
-#     if flag:
-#         print("存储成功!您的个人信息为：")
-#         uname = findByAccount(account)
-#         user = bank[uname]
-#         print(myinfo.format(account=user["account"],
-#                             username=uname,
-#                             password=user["password"],
-#                             country=user["country"],
-#                             province=user["province"],
-#                             street=user["street"],
-#                             door=user["door"],
-#                             money=user["money"],
-#                             bank_name=user["bank_name"]
-#                             ))
-#     else:
-#         print("对不起，您的个人信息不存在！请先开户后再次操作！")
-#
-# # 取钱
-# def takeMoney():
-#     account = inputHelp("账户")
-#     password =  inputHelp("密码")
-#     tmoney = inputHelp("取出金额","int")
-#
-#     f = bank_takeMoney(account,password,tmoney)
-#
-#     if f == 1:
-#         print("改用户不存在！")
-#     elif f == 2:
-#         print("密码错误！")
-#     elif f == 3:
-#         print("取款金额不足！")
-#     elif f == 0:
-#         print("取款成功！")
-#         bank_selectUser(account,password)
-#
-#
-# # 转账功能
-# def transformMoney():
-#     output = inputHelp("转出的账号")
-#     input = inputHelp("转入的账号")
-#     outputpass =  inputHelp("转出的密码")
-#     outputmoney = inputHelp("要转出的金额","int")
-#
-#     f = bank_transformMoney(output,input,outputpass,outputmoney)
-#
-#     if f == 1:
-#         print("转出或转入的账号不存在！")
-#     elif f == 2:
-#         print("输入密码错误！")
-#     elif f == 3:
-#         print("转账金额不足！")
-#     else:
-#         print("转账成功！")
-#         print("您的个人信息：")
-#         bank_selectUser(output,outputpass)
-#
-# # 查询账户方法
-# def selectUser():
-#     account = inputHelp("账号")
-#     password = inputHelp("密码")
-#
-#     bank_selectUser(account,password)
-#
-# # 核心程序
-# while True:
-#
-#     print_welcome()
-#     chose = inputHelp("选项")
-#     if isExists(chose,bank_choice):
-#         if chose  == "1":
-#             addUser()
-#         elif chose == "2":
-#             saveMoney()
-#         elif chose == "3":
-#             takeMoney()
-#         elif chose == "4":
-#             transformMoney()
-#         elif chose == "5":
-#             selectUser()
-#         elif chose == "6":
-#             print("Bye,Bye您嘞！！！！")
-#             break
-#     else:
-#         print("不存在改选项，别瞎弄！")
+	@data(*list4)
+	@unpack
+	def testTransformMoney(self, outac,inac,password,money,a, row):
+		re = bank_transformMoney(outac,inac,password,money)
+		# 断言
+		de1 = xlrd.open_workbook(filename='D:\Pythonproject\pythonproject1\pycode1\data.xls',
+								 encoding_override=True)
+		workbook = copy.copy(de1)
+		worksheet = workbook.get_sheet(3)
+		if re == a:
+			worksheet.write(row, 6, '通过')
+			workbook.save('data.xls')
+		else:
+			worksheet.write(row, 6, '未通过')
+			workbook.save('data.xls')
